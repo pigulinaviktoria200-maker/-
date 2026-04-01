78feb3d7f1c7065fe06954aea9ee4bba6764b8c1
pigulinaviktoria200-maker <pigulinaviktoria200@gmail.com>
2026-04-01 13:46:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        chunkSizeWarningLimit: 2000,
       }
     };
 });
